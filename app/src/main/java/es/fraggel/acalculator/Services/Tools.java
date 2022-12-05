0e2f9bcc290fe3b31cd225188d3f3d9792848470
fraggel <fraggelillo666@gmail.com>
2022-12-05 19:57:41 +0100

    cambiados textos meses

--- app/src/main/java/es/fraggel/acalculator/Services/Tools.java
+++ app/src/main/java/es/fraggel/acalculator/Services/Tools.java
@@ -95,18 +95,18 @@ public class Tools {
     }
 
     public static String toCharacterMonth(int month) {
-        if (month == 1) return "Jan";
+        if (month == 1) return "Ene";
         else if (month == 2) return "Feb";
         else if (month == 3) return "Mar";
-        else if (month == 4) return "Apr";
+        else if (month == 4) return "Abr";
         else if (month == 5) return "May";
         else if (month == 6) return "Jun";
         else if (month == 7) return "Jul";
-        else if (month == 8) return "Aug";
+        else if (month == 8) return "Ago";
         else if (month == 9) return "Sep";
         else if (month == 10) return "Oct";
         else if (month == 11) return "Nov";
-        else return "Dec";
+        else return "Dic";
     }
 
     public static String lastSeenProper(String lastSeenDate) {
