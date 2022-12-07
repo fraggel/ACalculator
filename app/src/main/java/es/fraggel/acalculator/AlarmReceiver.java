72130169b5dc314c2c1c40b1f76ab63eaf19ad3b
fraggel <fraggelillo666@gmail.com>
2022-12-07 09:26:09 +0100

    notificacion a 300segundos (5 min) y check de alarma aunque la creamos siempre

--- app/src/main/java/es/fraggel/acalculator/AlarmReceiver.java
+++ app/src/main/java/es/fraggel/acalculator/AlarmReceiver.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class AlarmReceiver extends BroadcastReceiver
 {
@@ -18,10 +19,21 @@ public class AlarmReceiver extends BroadcastReceiver
 
     public void setAlarm(Context context)
     {
-        AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
-        Intent i = new Intent(context, AlarmReceiver.class);
-        PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
-        assert am != null;
-        am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, (System.currentTimeMillis()/1000L + 300L) *1000L, pi);
+        boolean alarmUp = (PendingIntent.getBroadcast(context, 0,
+                new Intent(context,AlarmReceiver.class),
+                PendingIntent.FLAG_NO_CREATE) != null);
+
+        if (alarmUp)
+        {
+            Util.escribirLog("ALARMRECEIVER", "Alarma YA establecida no creamos más",context);
+        }else {
+
+        }
+            Util.escribirLog("ALARMRECEIVER", "Alarma establecida",context);
+            AlarmManager am =( AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+            Intent i = new Intent(context, AlarmReceiver.class);
+            PendingIntent pi = PendingIntent.getBroadcast(context, 0, i, 0);
+            assert am != null;
+            am.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, (System.currentTimeMillis()/1000L + 300L) *1000L, pi);
     }
 }
--- app/src/main/java/es/fraggel/acalculator/autostart.java
+++ app/src/main/java/es/fraggel/acalculator/autostart.java
@@ -15,9 +15,7 @@ public class autostart extends BroadcastReceiver
         if(arg1.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
             context.startService(new Intent(context,AppService.class));
             Util.setAlarm(context);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                //context.startForegroundService(new Intent(context, AppService.class));
-            }
+
         }
     }
 
